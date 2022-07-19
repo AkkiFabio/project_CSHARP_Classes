94afb362f4b01a6f9f0fd2cdf547ae96d0a15579
Fabio <akki.fabio@gmail.com>
2022-07-19 18:06:34 -0300

    Enviando atividade Classe

---
+++ Class_Construct_Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using Class_Construct_Exercise.Src;
+using System;
+
+namespace Class_Construct_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pokemon pokemon1 = new Pokemon("Kyurem", "Pressure", "Substitute", "Ice Beam", "Earth Power", "Roost");
+            Pokemon pokemon2 = new Pokemon("Landorus-Therian", "Intimidate", "Earthquake", "U-turn", "Stone Edge", "Defog");
+            Pokemon pokemon3 = new Pokemon("Kartana", "Beast Boost", "Swords Dance", "Leaf Blade", "Knock Off", "Sacred Sword");
+            Pokemon pokemon4 = new Pokemon("Kommo-o", "Bulletproof", "Clanging Scales", "Close Combat", "Clangorous Soul", "Flamethrower");
+
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Catarina's Pokemon Simulator");
+            //Console.WriteLine("Type 1 to see the available Pokémons or 2 to end the program");
+            Console.WriteLine("----------------------------");
+            //int ligar = int.Parse(Console.ReadLine());
+
+            
+                    Console.WriteLine(pokemon1.ToString());
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine(pokemon2.ToString());
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine(pokemon3.ToString());
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine(pokemon4.ToString());
+                    Console.WriteLine("----------------------");
+
+                    pokemon1.move2();
+                    pokemon2.move1();
+                    pokemon3.move2();
+                    pokemon4.move1();
+                    Console.WriteLine("----------------------");
+                    
+               
+
+            
+        }
+
+    }
+}
---
+++ Class_Construct_Exercise/Src/Pokemon.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Construct_Exercise.Src
+{
+    internal class Pokemon
+    {
+        public string name { get; set; }
+        public string ability  { get; set; }
+        public string skill1 { get; set; }
+        public string skill2 { get; set; }
+        public string skill3 { get; set; }
+        public string skill4 { get; set; }
+
+        public Pokemon()
+        {
+
+        }
+
+        public Pokemon(string aName, string aAbility, string aSkill1, string aSkill2, string aSkill3, string aSkill4)
+        {
+            name = aName;
+            ability = aAbility;
+            skill1 = aSkill1;
+            skill2 = aSkill2;
+            skill3 = aSkill3;
+            skill4 = aSkill4;
+        }
+        public void move1()
+        {
+            Console.WriteLine($"{name} used {skill1}!");
+        }
+        public void move2()
+        {
+            Console.WriteLine($"{name} used {skill2}!");
+        }
+        public void move3()
+        {
+            Console.WriteLine($"{name} used {skill3}!");
+        }
+        public void move4()
+        {
+            Console.WriteLine($"{name} used {skill4}!");
+        }
+        public void spacing()
+        {
+            Console.WriteLine("----------------------");
+        }
+        //Utilizei ToString ao invés disto
+        //public string moveset()
+        //{
+        //    return $" - Nome: {name}\n - Ability: {ability}\n - Skill 1: {skill1}\n - Skill 2: {skill2}\n - Skill 3: {skill3}\n - Skill 4: {skill4}\n";
+        //}
+        public override string ToString()
+        {
+            return $" - Nome: {name}\n - Ability: {ability}\n - Skill 1: {skill1}\n - Skill 2: {skill2}\n - Skill 3: {skill3}\n - Skill 4: {skill4}\n";
+        }
+    }
+}
